3dc0e0ec61eeb9b967af77aec4b5f1b983f6b9b9
Max Patiiuk <max@patii.uk>
2021-12-17 19:53:42 -0600

    Add WORMs taxa tool to docker composition

--- update-taxa.sh
+++ update-taxa.sh
@@ -1,5 +1,6 @@
 #!/bin/ash
 
-cd /home/specify/taxa_tree_gbif/back_end/ && /home/specify/venv/bin/python3 refresh_data.py
-cd /home/specify/taxa_tree_itis/back_end/ && /home/specify/venv/bin/python3 refresh_data.py
-cd /home/specify/taxa_tree_col/back_end/ && /home/specify/venv/bin/python3 refresh_data.py
+#cd /home/specify/taxa_tree_gbif/back_end/ && /home/specify/venv/bin/python3 refresh_data.py
+#cd /home/specify/taxa_tree_itis/back_end/ && /home/specify/venv/bin/python3 refresh_data.py
+#cd /home/specify/taxa_tree_col/back_end/ && /home/specify/venv/bin/python3 refresh_data.py
+cd /home/specify/taxa_tree_worms/back_end/ && /home/specify/venv/bin/python3 refresh_data.py
